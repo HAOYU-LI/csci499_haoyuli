f62b8d6b6bd6a3220dfd5717399442a2b78c9ba7
HAOYU-LI <haoyuli@usc.edu>
2020-02-03 18:29:56 +0000

    fixed issues based on comment

--- key_value_store/keyvalue_data_structure.h
+++ key_value_store/keyvalue_data_structure.h
@@ -11,18 +11,19 @@ using std::mutex;
 
 namespace kvstorage {	
 
-class Storage final{
- unordered_map<string, string> storage_map;
- mutex storage_mutex;
- public:
-  //put given (key, val) pair to Storage. If put successful, return true else return false;
-  bool Put(const string& key, const string& val);
-  //get val for given key from storage. If key doesn't exist, return empty string;
-  const string& Get(const string& key);
-  // delete value of a given key from storage. If key doesn't exist, return false, else true;
-  bool Deletekey(const string& key);
+class Storage final {
+  unordered_map<string, string> storage_map;
+  mutex storage_mutex;
+  public:
+    Storage() {}
+    //put given (key, val) pair to Storage. If put successful, return true;
+    bool Put(const string& key, const string& val);
+    //get val for given key from storage. If key doesn't exist, return empty string;
+    const string& Get(const string& key);
+    // delete value of a given key from storage. If key exists, return true;
+    bool Deletekey(const string& key);
 };
 
-}
+}//end of kvstorage namespace
 
 #endif
