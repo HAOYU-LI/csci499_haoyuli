0ca8c01f5055ac8fd5a2641766fb5975e9fda2f2
HAOYU-LI <haoyuli@usc.edu>
2020-02-13 18:15:37 +0000

    [warble-service] save development progress

---
+++ warble/warble_service_impl.h
@@ -0,0 +1,14 @@
+#ifndef WARBLE_SERVICE_IMPL_H
+#define WARBLE_SERVICE_IMPL_H
+
+#include <grpcpp/grpcpp.h>
+#include "warble_service.pb.h"
+
+#include "../key_value_store/key_value_data_structure.h"
+
+namespace warble {
+
+}
+
+
+#endif
